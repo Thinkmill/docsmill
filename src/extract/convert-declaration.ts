03a0b934d0d3b7071c4f8cb6568ed84cd173cb0e
mitchellhamilton <mitchell@hamil.town>
2021-09-10 13:30:02 +1000

    Refactoring

---
+++ src/extract/convert-declaration.ts
@@ -0,0 +1,302 @@
+import {
+  ArrowFunction,
+  ClassDeclaration,
+  ConstructorDeclaration,
+  EnumDeclaration,
+  EnumMember,
+  FunctionDeclaration,
+  InterfaceDeclaration,
+  JSDoc,
+  MethodDeclaration,
+  ModuleDeclaration,
+  ModuleDeclarationKind,
+  ModuledNode,
+  Node,
+  PrivateIdentifier,
+  PropertyDeclaration,
+  PropertySignature,
+  SourceFile,
+  ts,
+  TypeAliasDeclaration,
+  VariableDeclaration,
+} from "ts-morph";
+import { addExportedSymbol, collectSymbol, getRootSymbolName } from ".";
+import { ClassMember, SerializedSymbol } from "../lib/types";
+import { convertTypeNode } from "./convert-node";
+import { _convertType } from "./convert-type";
+import {
+  getDocs,
+  getTypeParameters,
+  getParameters,
+  getSymbolIdentifier,
+  getDocsFromJSDocNodes,
+  getObjectMembers,
+} from "./utils";
+
+export function convertDeclaration(decl: Node): SerializedSymbol {
+  if (decl instanceof TypeAliasDeclaration) {
+    const typeNode = decl.getTypeNode();
+    return {
+      kind: "type-alias",
+      name: decl.getName(),
+      docs: getDocs(decl),
+      typeParams: getTypeParameters(decl),
+      type: typeNode
+        ? convertTypeNode(typeNode)
+        : _convertType(decl.getType(), 0),
+    };
+  }
+  if (decl instanceof FunctionDeclaration) {
+    const returnTypeNode = decl.getReturnTypeNode();
+    return {
+      kind: "function",
+      // the only case where a function declaration doesn't have a name is when it's a default export
+      // (yes, function expressions never have to have names but this is a function declaration, not a function expression)
+      name: decl.getName() || "default",
+      parameters: getParameters(decl),
+      docs: getDocs(decl),
+      typeParams: getTypeParameters(decl),
+      returnType: returnTypeNode
+        ? convertTypeNode(returnTypeNode)
+        : _convertType(decl.getReturnType(), 0),
+    };
+  }
+  if (decl instanceof SourceFile) {
+    let jsDocs = getJsDocsFromSourceFile(decl);
+
+    // if you have a file that re-exports _everything_ from somewhere else
+    // then look at that place for jsdocs since e.g. Preconstruct
+    // generates a declaration file that re-exports from the actual place that might include a JSDoc comment
+    if (jsDocs.length === 0) {
+      let foundStar = false;
+      let sourceFile: undefined | SourceFile = undefined;
+      for (const exportDecl of decl.getExportDeclarations()) {
+        const file = exportDecl.getModuleSpecifierSourceFile();
+        if (!file || (sourceFile && file !== sourceFile)) {
+          sourceFile = undefined;
+          break;
+        }
+        sourceFile = file;
+        if (exportDecl.getNodeProperty("exportClause") === undefined) {
+          foundStar = true;
+        }
+      }
+      if (foundStar && sourceFile) {
+        jsDocs = getJsDocsFromSourceFile(sourceFile);
+      }
+    }
+
+    return {
+      kind: "module",
+      name: getRootSymbolName(decl.getSymbolOrThrow()) || decl.getFilePath(),
+      docs: getDocsFromJSDocNodes(jsDocs),
+      exports: collectExportsFromModule(decl),
+    };
+  }
+  if (decl instanceof VariableDeclaration) {
+    const typeNode = decl.getTypeNode();
+    const init = decl.getInitializer();
+    const variableStatement = decl.getVariableStatementOrThrow();
+
+    if (!typeNode && init instanceof ArrowFunction) {
+      const returnTypeNode = init.getReturnTypeNode();
+      return {
+        kind: "function",
+        name: decl.getName(),
+        parameters: getParameters(init),
+        docs: getDocs(variableStatement),
+        typeParams: getTypeParameters(init),
+        returnType: returnTypeNode
+          ? convertTypeNode(returnTypeNode)
+          : _convertType(init.getReturnType(), 0),
+      };
+    }
+    return {
+      kind: "variable",
+      name: decl.getName(),
+      docs: getDocs(variableStatement),
+      variableKind: variableStatement.getDeclarationKind(),
+      type: typeNode
+        ? convertTypeNode(typeNode)
+        : _convertType(decl.getType(), 0),
+    };
+  }
+  if (decl instanceof PropertySignature) {
+    const typeNode = decl.getTypeNode();
+    return {
+      kind: "variable",
+      name: decl.getName(),
+      docs: getDocs(decl),
+      variableKind: "const",
+      type: typeNode
+        ? convertTypeNode(typeNode)
+        : _convertType(decl.getType(), 0),
+    };
+  }
+  if (decl instanceof InterfaceDeclaration) {
+    return {
+      kind: "interface",
+      name: decl.getName(),
+      docs: getDocs(decl),
+      typeParams: getTypeParameters(decl),
+      extends: decl.getExtends().map((x) => convertTypeNode(x)),
+      members: getObjectMembers(decl),
+    };
+  }
+  if (decl instanceof ClassDeclaration) {
+    const extendsNode = decl.getExtends();
+    return {
+      kind: "class",
+      // just like function declarations, the only case where a class declaration doesn't have a name is when it's a default export
+      // (yes, class expressions never have to have names but this is a class declaration, not a class expression)
+      name: decl.getName() || "default",
+      docs: getDocs(decl),
+      typeParams: getTypeParameters(decl),
+      extends: extendsNode ? convertTypeNode(extendsNode) : null,
+      implements: decl.getImplements().map((x) => convertTypeNode(x)),
+      willBeComparedNominally: decl
+        .getMembers()
+        .some(
+          (member) =>
+            member.hasModifier("private") ||
+            member.hasModifier("protected") ||
+            (!(member instanceof ConstructorDeclaration) &&
+              member.getNameNode() instanceof PrivateIdentifier)
+        ),
+      constructors: decl.getConstructors().map((x) => {
+        return {
+          docs: getDocs(x),
+          parameters: getParameters(x),
+          typeParams: getTypeParameters(x),
+        };
+      }),
+      members: decl.getMembers().flatMap((member): ClassMember[] => {
+        if (
+          member.hasModifier("private") ||
+          member instanceof ConstructorDeclaration
+        ) {
+          return [];
+        }
+        if (member.getNameNode() instanceof PrivateIdentifier) {
+          return [];
+        }
+        if (member instanceof MethodDeclaration) {
+          // TODO: show protected
+          // (and have a tooltip explaining what protected does)
+
+          return [...member.getOverloads(), member].map((member) => {
+            const returnTypeNode = member.getReturnTypeNode();
+            return {
+              kind: "method",
+              docs: getDocs(member),
+              name: member.getName(),
+              static: member.isStatic(),
+              optional: member.hasQuestionToken(),
+              parameters: getParameters(member),
+              returnType: returnTypeNode
+                ? convertTypeNode(returnTypeNode)
+                : _convertType(member.getReturnType(), 0),
+              typeParams: getTypeParameters(member),
+            };
+          });
+        }
+        if (member instanceof PropertyDeclaration) {
+          const typeNode = member.getTypeNode();
+
+          return [
+            {
+              kind: "prop",
+              docs: getDocs(member),
+              name: member.getName(),
+              optional: member.hasQuestionToken(),
+              type: typeNode
+                ? convertTypeNode(typeNode)
+                : _convertType(member.getType(), 0),
+              static: member.isStatic(),
+              readonly: member.isReadonly(),
+            },
+          ];
+        }
+        return [{ kind: "unknown", content: member.getText() }];
+      }),
+    };
+  }
+  if (decl instanceof EnumDeclaration) {
+    return {
+      kind: "enum",
+      const: decl.isConstEnum(),
+      name: decl.getName(),
+      docs: getDocs(decl),
+      members: decl.getMembers().map((member) => {
+        const symbol = member.getSymbolOrThrow();
+        collectSymbol(symbol);
+        return getSymbolIdentifier(symbol);
+      }),
+    };
+  }
+  if (decl instanceof EnumMember) {
+    return {
+      kind: "enum-member",
+      name: decl.getName(),
+      docs: getDocs(decl),
+      value: decl.getValue() ?? null,
+    };
+  }
+  if (
+    decl instanceof ModuleDeclaration &&
+    decl.getDeclarationKind() === ModuleDeclarationKind.Namespace
+  ) {
+    return {
+      kind: "namespace",
+      name: decl.getName(),
+      docs: getDocs(decl),
+      exports: collectExportsFromModule(decl),
+    };
+  }
+  let docs = Node.isJSDocableNode(decl) ? getDocs(decl) : "";
+  let symbol = decl.getSymbolOrThrow();
+  console.log(symbol.getName(), decl.getKindName());
+  return {
+    kind: "unknown",
+    name: symbol.getName(),
+    docs,
+    content: decl.getText(),
+  };
+}
+
+function collectExportsFromModule(decl: ModuledNode) {
+  let exports: Record<string, string> = {};
+  for (const [
+    exportName,
+    exportedDeclarations,
+  ] of decl.getExportedDeclarations()) {
+    const decl = exportedDeclarations[0];
+    if (!decl) {
+      continue;
+    }
+    let innerSymbol = decl.getSymbolOrThrow();
+    innerSymbol = innerSymbol.getAliasedSymbol() || innerSymbol;
+    addExportedSymbol(innerSymbol);
+    collectSymbol(innerSymbol);
+    exports[exportName] = getSymbolIdentifier(innerSymbol);
+  }
+  return exports;
+}
+
+function getJsDocsFromSourceFile(decl: SourceFile) {
+  const jsDocs: JSDoc[] = [];
+  decl.forEachChild((node) => {
+    if (!!(node.compilerNode as any).jsDoc) {
+      const nodes: ts.JSDoc[] = (node.compilerNode as any).jsDoc ?? [];
+      const jsdocs: JSDoc[] = nodes.map((n) =>
+        (node as any)._getNodeFromCompilerNode(n)
+      );
+      for (const doc of jsdocs) {
+        if (doc.getTags().some((tag) => tag.getTagName() === "module")) {
+          jsDocs.push(doc);
+        }
+      }
+    }
+  });
+  return jsDocs;
+}
