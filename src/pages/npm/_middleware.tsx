d09e10098099957ef671f7d3b4dc65c1f1dd1fc9
mitchellhamilton <mitchell@hamil.town>
2021-11-20 15:29:37 +1000

    Catch errors

--- src/pages/npm/_middleware.tsx
+++ src/pages/npm/_middleware.tsx
@@ -2,14 +2,19 @@ import type { NextRequest } from "next/server";
 import { redirectToPkgVersion } from "../../npm/version-redirect";
 
 export async function middleware(req: NextRequest) {
-  const res = await redirectToPkgVersion(
-    Array.isArray(req.page.params!.pkg)
-      ? req.page.params!.pkg
-      : req.page.params!.pkg.split("/"),
-    "/npm"
-  );
-  if (res.kind === "handled" && "redirect" in res.result) {
-    return Response.redirect(res.result.redirect.destination, 307);
+  try {
+    const res = await redirectToPkgVersion(
+      Array.isArray(req.page.params!.pkg)
+        ? req.page.params!.pkg
+        : req.page.params!.pkg.split("/"),
+      "/npm"
+    );
+    return new Response(JSON.stringify({ res }, null, 2));
+  } catch (err: any) {
+    return new Response(JSON.stringify(err.toString() + err.stack, null, 2));
   }
-  return new Response(JSON.stringify({ res }, null, 2));
+
+  // if (res.kind === "handled" && "redirect" in res.result) {
+  //   return Response.redirect(res.result.redirect.destination, 307);
+  // }
 }
