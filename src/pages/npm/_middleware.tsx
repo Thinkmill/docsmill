16e9c0eaca65412938a2889bac4dfd002b0a0dcd
mitchellhamilton <mitchell@hamil.town>
2021-11-20 15:34:28 +1000

    Try a thing

--- src/pages/npm/_middleware.tsx
+++ src/pages/npm/_middleware.tsx
@@ -2,17 +2,15 @@ import type { NextRequest } from "next/server";
 import { redirectToPkgVersion } from "../../npm/version-redirect";
 
 export async function middleware(req: NextRequest) {
-  try {
-    const res = await redirectToPkgVersion(
-      Array.isArray(req.page.params!.pkg)
-        ? req.page.params!.pkg
-        : req.page.params!.pkg.split("/"),
-      "/npm"
-    );
-    return new Response(JSON.stringify({ res }, null, 2));
-  } catch (err: any) {
-    return new Response(JSON.stringify(err.toString() + err.stack, null, 2));
-  }
+  const res = await redirectToPkgVersion(
+    Array.isArray(req.page.params!.pkg)
+      ? req.page.params!.pkg
+      : req.page.params!.pkg.split("/"),
+    "/npm"
+  );
+  return new Response(
+    JSON.stringify({ res, keys: Object.keys(globalThis) }, null, 2)
+  );
 
   // if (res.kind === "handled" && "redirect" in res.result) {
   //   return Response.redirect(res.result.redirect.destination, 307);
